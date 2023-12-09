8ff1baac0a99b0c869aac9c68c515f5eefa81b81
xiluohe <42448015+xiluohe@users.noreply.github.com>
2023-12-09 01:58:17 -0800

    Update training_loop.py

--- DiffAugment-stylegan2-pytorch/training/training_loop.py
+++ DiffAugment-stylegan2-pytorch/training/training_loop.py
@@ -121,9 +121,7 @@ def training_loop(
 ):
     # Initialize.
     start_time = time.time()
-    # XILUO EDITED 
     device = torch.device('cuda', rank)
-    # device = torch.device('cuda', 2)
     np.random.seed(random_seed * num_gpus + rank)
     torch.manual_seed(random_seed * num_gpus + rank)
     torch.backends.cudnn.benchmark = cudnn_benchmark    # Improves training speed.
