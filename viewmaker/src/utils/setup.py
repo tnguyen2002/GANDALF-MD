abf91a8100583c0df8cb69d928c9f119c528292a
xiluohe <42448015+xiluohe@users.noreply.github.com>
2023-12-09 12:45:07 -0800

    Update setup.py

--- viewmaker/src/utils/setup.py
+++ viewmaker/src/utils/setup.py
@@ -12,7 +12,7 @@ from logging.handlers import RotatingFileHandler
 
 from src.utils.utils import load_json, save_json
 
-DEFAULT_EXP_BASE = os.path.join(os.path.dirname(__file__), '~/VMDGan/viewmaker_models/')
+DEFAULT_EXP_BASE = os.path.join(os.path.dirname(__file__), '~/experiments/')
 
 
 def makedirs(dir_list):
