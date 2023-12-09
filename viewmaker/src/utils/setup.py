50ddc485fef8963610e8ebb1b3e8645845f625b1
xiluohe <42448015+xiluohe@users.noreply.github.com>
2023-12-09 01:46:09 -0800

    Update setup.py

--- viewmaker/src/utils/setup.py
+++ viewmaker/src/utils/setup.py
@@ -12,7 +12,7 @@ from logging.handlers import RotatingFileHandler
 
 from src.utils.utils import load_json, save_json
 
-DEFAULT_EXP_BASE = os.path.join(os.path.dirname(__file__), '/home/xiluohe/VMDGan/viewmaker_models/gan_trained_vm_models/')
+DEFAULT_EXP_BASE = os.path.join(os.path.dirname(__file__), '~/VMDGan/viewmaker_models/')
 
 
 def makedirs(dir_list):
